8e607742e81fe2b67005d92469982c3c91cb5c2b
David Hoffman <me@davidhoffman.ca>
2016-10-05 22:01:47 -0400

    added png generation

--- gen_slides/gen_slides.py
+++ gen_slides/gen_slides.py
@@ -5,12 +5,16 @@ import errno
 import requests
 from uuid import uuid4
 
+import subprocess
 from jinja2.environment import Environment
 from jinja2.loaders import PackageLoader
 
 parser = argparse.ArgumentParser(description='Generate YouTube video slides from JSON data')
 parser.add_argument('schedule_path', help='Path to schedule.json')
 parser.add_argument('--talk-root', dest='talk_root', help='URL to the root directory for JSON talks', default='https://2016.pycon.ca/en/schedule/')
+parser.add_argument('--webkit2png-path', dest='webkit2png_path', help='Path for webkit2png executable', default='/usr/local/bin/webkit2png')
+parser.add_argument('--height', dest='height', help='Height for the slides', default='1024')
+parser.add_argument('--width', dest='width', help='Width for the slides', default='768')
 
 args = parser.parse_args()
 
@@ -30,33 +34,31 @@ def generate_html(slide):
 
         env = Environment(loader=PackageLoader('gen_slides', 'templates'))
         template = env.get_template('slide.html')
-        print(template.render(slide=slide))
-        return template.render(slide=slide)
-        # html = loader.render_to_string('slides/slide.html', slide)
-        # html_file = open(html_path, 'wb')
-        # html_file.write(html.encode('utf8'))
-        # html_file.close()
-        # html_paths.append(html_path)
-#
-#
-# def html_to_png(html_path):
-#     png_path = html_path.replace('/html/', '/png/') \
-#         .replace('.html', '')
-#     png_dir, png_name = os.path.split(png_path)
-#     subprocess.check_call([
-#         settings.WEBKIT2PNG_PATH,
-#         '-F',  # just full size image
-#         '-W',
-#         str(settings.SLIDE_WIDTH),
-#         '-H',
-#         str(settings.SLIDE_HEIGHT),
-#         '-D',
-#         png_dir,
-#         '-o',
-#         png_name,
-#         html_path,
-#     ])
-#     return png_path + '-full.png'
+        html = template.render(slide=slide)
+        html_file = open(html_path, 'wb')
+        html_file.write(html.encode('utf8'))
+        html_file.close()
+        return html_path
+
+
+def html_to_png(html_path):
+    png_path = html_path.replace('/html/', '/png/') \
+        .replace('.html', '')
+    png_dir, png_name = os.path.split(png_path)
+    subprocess.check_call([
+        args.webkit2png_path,
+        '-F',  # just full size image
+        '-W',
+        args.width,
+        '-H',
+        args.height,
+        '-D',
+        png_dir,
+        '-o',
+        png_name,
+        html_path,
+    ])
+    return png_path + '-full.png'
 
 
 def get_slides():
@@ -88,4 +90,5 @@ html_dir = os.path.join(base_dir, 'html')
 os.makedirs(html_dir)
 
 slides = get_slides()
-html_files = map(generate_html, slides)
+html_paths = map(generate_html, slides)
+png_paths = map(html_to_png, html_paths)
